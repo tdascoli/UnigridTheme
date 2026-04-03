3dfddac0a57d125e57e8f1037e8af2648e4e142e
tdascoli <R0hrch@ch>
2026-04-03 14:39:26 +0200

    add open-type features

--- unigrid-theme/src/commonMain/kotlin/com/unigrid/theme/Typography.kt
+++ unigrid-theme/src/commonMain/kotlin/com/unigrid/theme/Typography.kt
@@ -32,6 +32,7 @@ fun InterFontFamily(): FontFamily = FontFamily(
 @Composable
 fun UnigridTypography(): Typography {
     val inter = InterFontFamily()
+    val fontFeatures = "liga, calt, ss07"
     return Typography(
         displayLarge = TextStyle(
             fontFamily = inter,
@@ -39,90 +40,105 @@ fun UnigridTypography(): Typography {
             fontSize = 44.sp,
             lineHeight = 52.sp,
             letterSpacing = (-0.35).sp,
+            fontFeatureSettings = fontFeatures
         ),
         displayMedium = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Bold,
             fontSize = 31.sp,
             lineHeight = 38.sp,
+            fontFeatureSettings = fontFeatures
         ),
         displaySmall = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Bold,
             fontSize = 26.sp,
             lineHeight = 32.sp,
+            fontFeatureSettings = fontFeatures
         ),
         headlineLarge = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Bold,
             fontSize = 24.sp,
             lineHeight = 30.sp,
+            fontFeatureSettings = fontFeatures
         ),
         headlineMedium = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Bold,
             fontSize = 22.sp,
             lineHeight = 28.sp,
+            fontFeatureSettings = fontFeatures
         ),
         headlineSmall = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp,
             lineHeight = 26.sp,
+            fontFeatureSettings = fontFeatures
         ),
         titleLarge = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.SemiBold,
             fontSize = 22.sp,
             lineHeight = 28.sp,
+            fontFeatureSettings = fontFeatures
         ),
         titleMedium = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.SemiBold,
             fontSize = 20.sp,
             lineHeight = 26.sp,
+            fontFeatureSettings = fontFeatures
         ),
         titleSmall = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.SemiBold,
             fontSize = 18.sp,
             lineHeight = 24.sp,
+            fontFeatureSettings = fontFeatures
         ),
         bodyLarge = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Normal,
             fontSize = 20.sp,
             lineHeight = 30.sp,
+            fontFeatureSettings = fontFeatures
         ),
         bodyMedium = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Normal,
             fontSize = 18.sp,
             lineHeight = 26.sp,
+            fontFeatureSettings = fontFeatures
         ),
         bodySmall = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Normal,
             fontSize = 16.sp,
             lineHeight = 22.sp,
+            fontFeatureSettings = fontFeatures
         ),
         labelLarge = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Bold,
             fontSize = 18.sp,
             lineHeight = 24.sp,
+            fontFeatureSettings = fontFeatures
         ),
         labelMedium = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Bold,
             fontSize = 16.sp,
             lineHeight = 20.sp,
+            fontFeatureSettings = fontFeatures
         ),
         labelSmall = TextStyle(
             fontFamily = inter,
             fontWeight = FontWeight.Bold,
             fontSize = 14.sp,
             lineHeight = 18.sp,
+            fontFeatureSettings = fontFeatures
         ),
     )
 }
